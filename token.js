12becc4821b7e2e376f12849a0ba908f7e704d29
veena vijayan <veenavijayan38@gmail.com>
2022-08-19 15:16:40 +0530

    objectId validation in all apis

--- token.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-const firebase=require('firebase')
-const getToken=async ()=>{
-const firebaseToken=await firebase.messaging.getToken()
-console.log('token',firebaseToken)
-}
