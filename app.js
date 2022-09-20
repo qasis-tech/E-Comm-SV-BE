6502f7e9176bfca479c3ab5e5531a5bdb0d0efd2
Sabeer ali <sabeer@sectorqube.com>
2022-09-20 09:03:33 +0530

    port changed

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-import express from 'express'
+import express from "express";
 const app = express();
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser'
-import adminRouter from './src/routes/adminRouter.js'
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import adminRouter from "./src/routes/adminRouter.js";
 // import dbCon from "./src/config/connections/connection.js";
-import cors from 'cors'
+import cors from "cors";
 app.use(cors({ origin: true, credentials: true }));
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-mongoose.connect(process.env.DB_BASE_URL, {
-  useNewUrlParser: true,
-})
-.then(() => {
-  console.log(`DB Connected...!`);
-})
-.catch((err) => {
-  console.log(`Could not connect to the database. Exiting now...${err}`);
-});
+mongoose
+  .connect(process.env.DB_BASE_URL, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log(`DB Connected...!`);
+  })
+  .catch((err) => {
+    console.log(`Could not connect to the database. Exiting now...${err}`);
+  });
 app.use(
   bodyParser.urlencoded({
     extended: true,
@@ -29,5 +30,4 @@ app.use("/", adminRouter);
 const port = process.env.PORT || 7000;
 app.listen(port, () => {
   console.log("Server is listening on port ", port);
-  
 });
